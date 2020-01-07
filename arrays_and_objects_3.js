74e7b14727d75a23a40e2428964a9e5c5415ec93
Brian Bachow <bmbachow@gmail.com>
2020-01-07 14:19:36 -0800

    arrays_and_objects_3

---
+++ arrays_and_objects_3.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const mainProperty = {
+  meals : ['breakfast' , 'second breakfast' , 'elevenses' , 'lunch' , 'afternoon tea' , 'dinner' , 'supper']
+};
+
+console.log(mainProperty.meals[3])
